986c9731749a6d548613426435aff564378c5b0e
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2017-06-17 19:47:35 +0200

    Initial Commit

---
+++ prRSS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prRSS
+//
+//  Created by Miguel Gallego on 17/6/17.
+//  Copyright © 2017 Miguel Gallego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
